f8bbbb12b85ee5934ddbd80d5eaecdc4e86bd6af
Max Lödige <max@loedige-net.de>
2025-01-08 11:06:38 +0100

    Added ability to load plugins

--- mujoco_ros2_control/src/mujoco_ros2_control_node.cpp
+++ mujoco_ros2_control/src/mujoco_ros2_control_node.cpp
@@ -5,9 +5,145 @@
 #include "mujoco_ros2_control/mujoco_ros2_control.hpp"
 #include "mujoco_ros2_control/mujoco_rendering.hpp"
 
+#define MUJOCO_PLUGIN_DIR "mujoco_plugin"
+
 // MuJoCo data structures
 mjModel* mujoco_model = nullptr;
 mjData* mujoco_data = nullptr;
+
+std::string getExecutableDir() {
+#if defined(_WIN32) || defined(__CYGWIN__)
+  constexpr char kPathSep = '\\';
+  std::string realpath = [&]() -> std::string {
+    std::unique_ptr<char[]> realpath(nullptr);
+    DWORD buf_size = 128;
+    bool success = false;
+    while (!success) {
+      realpath.reset(new(std::nothrow) char[buf_size]);
+      if (!realpath) {
+        std::cerr << "cannot allocate memory to store executable path\n";
+        return "";
+      }
+
+      DWORD written = GetModuleFileNameA(nullptr, realpath.get(), buf_size);
+      if (written < buf_size) {
+        success = true;
+      } else if (written == buf_size) {
+        // realpath is too small, grow and retry
+        buf_size *=2;
+      } else {
+        std::cerr << "failed to retrieve executable path: " << GetLastError() << "\n";
+        return "";
+      }
+    }
+    return realpath.get();
+  }();
+#else
+  constexpr char kPathSep = '/';
+#if defined(__APPLE__)
+  std::unique_ptr<char[]> buf(nullptr);
+  {
+    std::uint32_t buf_size = 0;
+    _NSGetExecutablePath(nullptr, &buf_size);
+    buf.reset(new char[buf_size]);
+    if (!buf) {
+      std::cerr << "cannot allocate memory to store executable path\n";
+      return "";
+    }
+    if (_NSGetExecutablePath(buf.get(), &buf_size)) {
+      std::cerr << "unexpected error from _NSGetExecutablePath\n";
+    }
+  }
+  const char* path = buf.get();
+#else
+  const char* path = "/proc/self/exe";
+#endif
+  std::string realpath = [&]() -> std::string {
+    std::unique_ptr<char[]> realpath(nullptr);
+    std::uint32_t buf_size = 128;
+    bool success = false;
+    while (!success) {
+      realpath.reset(new(std::nothrow) char[buf_size]);
+      if (!realpath) {
+        std::cerr << "cannot allocate memory to store executable path\n";
+        return "";
+      }
+
+      std::size_t written = readlink(path, realpath.get(), buf_size);
+      if (written < buf_size) {
+        realpath.get()[written] = '\0';
+        success = true;
+      } else if (written == -1) {
+        if (errno == EINVAL) {
+          // path is already not a symlink, just use it
+          return path;
+        }
+
+        std::cerr << "error while resolving executable path: " << strerror(errno) << '\n';
+        return "";
+      } else {
+        // realpath is too small, grow and retry
+        buf_size *= 2;
+      }
+    }
+    return realpath.get();
+  }();
+#endif
+
+  if (realpath.empty()) {
+    return "";
+  }
+
+  for (std::size_t i = realpath.size() - 1; i > 0; --i) {
+    if (realpath.c_str()[i] == kPathSep) {
+      return realpath.substr(0, i);
+    }
+  }
+
+  // don't scan through the entire file system's root
+  return "";
+}
+
+void scanPluginLibraries() {
+  // check and print plugins that are linked directly into the executable
+
+  // define platform-specific strings
+#if defined(_WIN32) || defined(__CYGWIN__)
+  const std::string sep = "\\";
+#else
+  const std::string sep = "/";
+#endif
+
+
+  // try to open the ${EXECDIR}/MUJOCO_PLUGIN_DIR directory
+  // ${EXECDIR} is the directory containing the simulate binary itself
+  // MUJOCO_PLUGIN_DIR is the MUJOCO_PLUGIN_DIR preprocessor macro
+  const std::string executable_dir = getExecutableDir();
+  if (executable_dir.empty()) {
+    return ;
+  }
+  const std::string plugin_dir = getExecutableDir() + sep + MUJOCO_PLUGIN_DIR;
+  mj_loadAllPluginLibraries(plugin_dir.c_str(),
+      +[](const char* filename, int first, int count) {
+        // log += "Plugins registered by library '" + (std::string)filename + "':\n";
+        std::printf("Plugins registered by library '%s':\n", filename);
+        for (int i = first; i < first + count; ++i) {
+          // log += "    " + (std::string)mjp_getPluginAtSlot(i)->name + "\n";
+          std::printf("    %s\n", mjp_getPluginAtSlot(i)->name);
+        }
+      }
+  );
+
+  // std::string log = "";
+  // int nplugin = mjp_pluginCount();
+  // if (nplugin) {
+  //   log + "Registered plugins:\n";
+  //   for (int i = 0; i < nplugin; ++i) {
+  //     log +=  "    " + (std::string)mjp_getPluginAtSlot(i)->name + "\n";
+  //   }
+  // }
+  //   return log;
+}
 
 // main function
 int main(int argc, const char** argv) {
@@ -17,6 +153,9 @@ int main(int argc, const char** argv) {
 
   RCLCPP_INFO_STREAM(node->get_logger(), "Initializing mujoco_ros2_control node...");
   auto model_path = node->get_parameter("mujoco_model_path").as_string();
+
+  // RCLCPP_INFO_STREAM(node->get_logger(),());
+  scanPluginLibraries();
 
   // load and compile model
   char error[1000] = "Could not load binary model";
